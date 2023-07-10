d6b338bd3b9ab91026ac44f2287f88044243ab6f
php5185 <php5185@rit.edu>
2023-07-09 21:31:42 -0400

    add attributes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,137 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
+import { List } from 'antd'
+
+const CONST_INTELLIGENCE = 'Intelligence';
 
 
 function App() {
-  const [num, setNum] = useState(0);
+  //const [num, setNum] = useState(0);
+  const [attribute, setAttribute] = useState({});
+  const [attributeCount, setAttributeCount] = useState(0);
+
+  const [modifier, setModifier] = useState({});
+  const [points, setPoints] = useState(10);
+  const [skillPoints, setSkillPoints] = useState({});
+  const [pointsCount, setPointsCount] = useState(0);
+  const [classes, setClasses] = useState({});
+  const [classList, setClassList] = useState([]);
+
+  const initCharacter = async () => {
+
+    //   const { data } = await axios.get(
+    //     `http://localhost:3001/random-quiz`
+    // );
+
+    // if data {
+
+    // }
+    // else{
+
+    // }
+
+    let startAttributes = {}
+    let startModifiers = {}
+
+    let startAttributeCount = 0;
+
+    ATTRIBUTE_LIST.forEach(element => {
+      startAttributes[element] = 10;
+      startModifiers[element] = 0;
+      startAttributeCount += 10;
+    });
+    setAttribute(startAttributes);
+    setModifier(startModifiers);
+
+    let startPoints = 10 + (4 * modifier[CONST_INTELLIGENCE]);
+    setPoints(startPoints);
+
+    let startSkills = {};
+
+    SKILL_LIST.forEach(element => {
+      startSkills[element.name] = 0;
+    });
+
+    setSkillPoints(startSkills);
+
+    let startClases = {}
+    let allClasses = [];
+
+    for (const key in CLASS_LIST) {
+      startClases[key] = false;
+      allClasses.push(key);
+    }
+
+    setClasses(startClases);
+    setClassList(allClasses);
+    setAttributeCount(startAttributeCount);
+    setPointsCount(0);
+
+  }
+
+  useEffect(() => {
+    initCharacter();
+  }, []);
+
+
+  const upgradeAttribute = event => {
+    //check points
+    if (attributeCount === 70) {
+      alert("Maximun 70 Attribute points allowed.");
+      return;
+    }
+  
+    setAttributeCount((prevCount => (prevCount+1)));
+    const val = event.currentTarget.id;
+  
+    setAttribute((prevAttribute) => ({
+      ...prevAttribute,
+     [val]: prevAttribute[val] + 1
+    }))
+    
+  };
+
+  const downgradeAttribute = event => {
+    setAttributeCount((prevCount => (prevCount-1)));
+    const val = event.currentTarget.id;
+  
+    setAttribute((prevAttribute) => ({
+      ...prevAttribute,
+     [val]: prevAttribute[val] - 1
+    }))
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Coding Exercise</h1>
+        <h1>React Coding Exercise -- Paola Peralta</h1>
       </header>
       <section className="App-section">
-        <div>
+        {/* <div>
           Value:
           {num}
           <button>+</button>
           <button>-</button>
+        </div> */}
+
+        <div className='Attributes'>
+        <List
+            size="small"
+            header={<h2>Attributes</h2>}
+            bordered
+            dataSource={ATTRIBUTE_LIST}
+            renderItem={(key) =>  <List.Item>
+              {key}: {attribute[key]} (modifier: {modifier[key]})
+              <button id={key} onClick={upgradeAttribute}>+</button>
+              <button id={key} onClick={downgradeAttribute}>-</button>
+              </List.Item>}
+        />
+
         </div>
+
+
       </section>
     </div>
   );
